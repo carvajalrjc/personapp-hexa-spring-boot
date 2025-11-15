746f8add534e711c585611dd3cea9f5941fc30aa
Juan Camilo Carvajal Rodríguez <jcarvajalrodriguez2003@gmail.com>
2025-11-15 14:41:07 -0500

    fix: use composite ID for Study deletion in MongoDB
    - Change delete and findById methods to use composite ID (personId-professionId)
    - Remove unused autowired fields and imports
    - Fix 500 error when deleting Study from MongoDB

--- mongo-output-adapter/src/main/java/co/edu/javeriana/as/personapp/mongo/adapter/StudyOutputAdapterMongo.java
+++ mongo-output-adapter/src/main/java/co/edu/javeriana/as/personapp/mongo/adapter/StudyOutputAdapterMongo.java
@@ -12,14 +12,8 @@ import co.edu.javeriana.as.personapp.application.port.out.StudyOutputPort;
 import co.edu.javeriana.as.personapp.common.annotations.Adapter;
 import co.edu.javeriana.as.personapp.domain.Study;
 import co.edu.javeriana.as.personapp.mongo.document.EstudiosDocument;
-import co.edu.javeriana.as.personapp.mongo.document.PersonaDocument;
-import co.edu.javeriana.as.personapp.mongo.document.ProfesionDocument;
 import co.edu.javeriana.as.personapp.mongo.mapper.EstudiosMapperMongo;
-import co.edu.javeriana.as.personapp.mongo.mapper.PersonaMapperMongo;
-import co.edu.javeriana.as.personapp.mongo.mapper.ProfesionMapperMongo;
 import co.edu.javeriana.as.personapp.mongo.repository.EstudioRepositoryMongo;
-import co.edu.javeriana.as.personapp.mongo.repository.PersonaRepositoryMongo;
-import co.edu.javeriana.as.personapp.mongo.repository.ProfesionRepositoryMongo;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -31,18 +25,6 @@ public class StudyOutputAdapterMongo implements StudyOutputPort {
 
 	@Autowired
 	private EstudiosMapperMongo estudiosMapperMongo;
-
-	@Autowired
-	private PersonaRepositoryMongo personaRepositoryMongo;
-
-	@Autowired
-	private PersonaMapperMongo personaMapperMongo;
-
-	@Autowired
-	private ProfesionRepositoryMongo profesionRepositoryMongo;
-
-	@Autowired
-	private ProfesionMapperMongo profesionMapperMongo;
 
 	@Override
 	public Study save(Study study) {
@@ -60,15 +42,11 @@ public class StudyOutputAdapterMongo implements StudyOutputPort {
 	@Override
 	public Boolean delete(Integer identification, Integer idPerson) {
 		log.debug("Into delete on Adapter MongoDB");
-		Optional<PersonaDocument> persona = personaRepositoryMongo.findById(idPerson);
-		Optional<ProfesionDocument> profesion = profesionRepositoryMongo.findById(identification);
-		if (persona.isPresent() && profesion.isPresent()) {
-			EstudiosDocument estudio = estudioRepositoryMongo
-					.findByPrimaryPersonaAndPrimaryProfesion(persona.get(), profesion.get()).orElse(null);
-			if (estudio != null) {
-				estudioRepositoryMongo.delete(estudio);
-				return true;
-			}
+		String compositeId = idPerson + "-" + identification;
+		Optional<EstudiosDocument> estudio = estudioRepositoryMongo.findById(compositeId);
+		if (estudio.isPresent()) {
+			estudioRepositoryMongo.delete(estudio.get());
+			return true;
 		}
 		return false;
 	}
@@ -76,14 +54,10 @@ public class StudyOutputAdapterMongo implements StudyOutputPort {
 	@Override
 	public Study findById(Integer identification, Integer idPerson) {
 		log.debug("Into findById on Adapter MongoDB");
-		Optional<PersonaDocument> persona = personaRepositoryMongo.findById(idPerson);
-		Optional<ProfesionDocument> profesion = profesionRepositoryMongo.findById(identification);
-		if (persona.isPresent() && profesion.isPresent()) {
-			EstudiosDocument estudio = estudioRepositoryMongo
-					.findByPrimaryPersonaAndPrimaryProfesion(persona.get(), profesion.get()).orElse(null);
-			if (estudio != null) {
-				return estudiosMapperMongo.fromAdapterToDomain(estudio);
-			}
+		String compositeId = idPerson + "-" + identification;
+		Optional<EstudiosDocument> estudio = estudioRepositoryMongo.findById(compositeId);
+		if (estudio.isPresent()) {
+			return estudiosMapperMongo.fromAdapterToDomain(estudio.get());
 		}
 		return null;
 	}
